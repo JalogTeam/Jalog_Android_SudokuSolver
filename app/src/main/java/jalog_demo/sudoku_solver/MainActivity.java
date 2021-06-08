24eb093d4e5d1d6c504e5153e64c4a7b8b84841a
Ari Okkonen <ari.okkonen@gmail.com>
2021-06-08 23:20:44 +0300

    Sudoku ratkeaa kännykässä

--- app/src/main/java/jalog_demo/sudoku_solver/MainActivity.java
+++ app/src/main/java/jalog_demo/sudoku_solver/MainActivity.java
@@ -1,5 +1,8 @@
 package jalog_demo.sudoku_solver;
+import android.content.Context;
+import android.content.res.AssetManager;
 import io.github.JalogTeam.jalog.Jalog;
+import io.github.JalogTeam.jalog.Pro_Term;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,16 +14,27 @@ import android.os.CountDownTimer;
 
 
 import android.os.Bundle;
+import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
   class sudoku_cell {
-    View view;
+    TextView view;
     int value;
   };
   static sudoku_cell[][] sudoku_field;
   static TextView wait_message;
   static float cell_translation_one;
+  static AssetManager am = null;
+  static String convert_number(int v) {
+    String disp_string;
 
+    if (v == 0) {
+      disp_string = " ";
+    } else {
+      disp_string = "" + (char)('0' + v);
+    }
+    return disp_string;
+  }
   static View.OnClickListener click_listener = new View.OnClickListener() {
     public void onClick(View v) {
       // your handler code here
@@ -29,16 +43,19 @@ public class MainActivity extends AppCompatActivity {
       Button x = (Button)v;
       sudoku_cell cell = (sudoku_cell)x.getTag();
 
-      cell.value = cell.value + 1;
+      cell.value = (cell.value + 1) % 10;
+/*
       if (cell.value > 9) {
         cell.value = 0;
-        disp_string = "_";
+        disp_string = " ";
       } else {
         symbol = (char)('0' + cell.value);
         disp_string = "" + symbol;
       }
+ */
 //        push_button11.setText(disp_string);
-      x.setText(disp_string);
+//      x.setText(disp_string);
+      x.setText(convert_number(cell.value));
     }
   };
 
@@ -71,7 +88,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void onFinish() {
-          Jalog myJalog = new Jalog();
+          Jalog myJalog = new Jalog(am);
+
+//          Context mContext = getApplicationContext();
+//          InputStream is = mContext.getAssets().open("sudoku_solver_component.pro");
+
+
+
+          myJalog.consult_file("res:sudoku_solver_component.pro");
+
+          solve_sudoku(myJalog);
+
           myJalog.dispose();
 
           wait_message.setVisibility(View.INVISIBLE);
@@ -80,6 +107,59 @@ public class MainActivity extends AppCompatActivity {
 
     }
   };
+
+  static void solve_sudoku(Jalog myJalog) {
+
+    int i, j, value;
+    Jalog.Term line[] = new Jalog.Term[9];
+    Jalog.Term matrix[] = new Jalog.Term[9];
+    Jalog.Term board;
+    Jalog.Term element;
+
+//  Preparation of the problem
+    
+    for (i = 0; i < 9; i++) {
+      for (j = 0; j < 9; j++) {
+//        value = sudoku_problem[i][j];
+        value = sudoku_field[i][j].value;
+        if(value == 0) {
+          line[j] = Jalog.open();
+        } else {
+          line[j] = Jalog.integer(value);
+        }
+      }
+      matrix[i] = Jalog.list(line);
+    }
+    board = Jalog.list(matrix);
+
+//  Finding the solution
+  
+    try {
+      if (myJalog.call("sudoku", board))
+      { // Getting the solution
+        matrix = board.getElements();
+        for (i = 0; i < 9; i++) {
+          line = matrix[i].getElements();
+          for (j = 0; j < 9; j++) {
+            element = line[j];
+            if (element.getType() == Jalog.INTEGER) {
+//              sudoku_solution[i][j] = (int)element.getIntegerValue();
+              sudoku_field[i][j].value = (int)element.getIntegerValue();
+              sudoku_field[i][j].view.setText(convert_number(sudoku_field[i][j].value));
+
+            }
+          }
+        }
+      } else { // fail: Report solution not found
+        System.out.println("x");
+      }
+    } catch (Jalog.Exit e) { // Report exception
+      System.out.println("x");
+    }
+  }
+
+
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +170,23 @@ public class MainActivity extends AppCompatActivity {
     sudoku_field = new sudoku_cell[9][];
 //    sudoku_field = new sudoku_cell[9][9];
     setContentView(R.layout.activity_main);
+    am = getResources().getAssets();
+/*
+    final InputStream   is;
+    final AssetManager am = getResources().getAssets();
+    String line;
+    try {
+
+      is = am.open("sudoku_solver_component.pro");
+
+      Reader input =
+          new InputStreamReader(is, "UTF-8");
+      BufferedReader file1 = new BufferedReader(input);
+      line = file1.readLine();
+    } catch (Exception e) {
+    }
+*/
+
 
     wait_message = (TextView) findViewById(R.id.wait_message);
     wait_message.setVisibility(View.INVISIBLE);
@@ -118,12 +215,16 @@ public class MainActivity extends AppCompatActivity {
         push_button.setLayoutParams(new ViewGroup.LayoutParams(button_width, button_height));
         push_button.setTranslationX(j*cell_translation_one);
         push_button.setTranslationY(i*cell_translation_one);
-        push_button.setText("" + (i + 1) + (j + 1));
         push_button.setTextSize(20);
 //    push_button31.setZ(99);
         main_viewgroup.addView(push_button);
         sudoku_field[i][j].view = push_button;
+        /*
         sudoku_field[i][j].value = 10*(i+1) + j + 1;
+        push_button.setText("" + (i + 1) + (j + 1));
+        */
+        sudoku_field[i][j].value = 0;
+        push_button.setText(" ");
         push_button.setTag(sudoku_field[i][j]);
 
         push_button.setOnClickListener(click_listener);
